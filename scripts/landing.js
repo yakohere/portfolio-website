a977a20ab212b8c02151fd6b0dabd5bca62dfa7e
yakohere <yakhyoismoildjonov@gmail.com>
2021-09-11 09:57:16 +0500

    menu - skills animation - header backgound

--- scripts/landing.js
+++ scripts/landing.js
@@ -5,3 +5,10 @@ function showMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show-menu")
 }
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+
+    header.style.background = window.scrollY > 700 ? "rgba(213, 196, 85, 0.9)" : "transparent"
+})
--- scripts/slowNumber.js
+++ scripts/slowNumber.js
@@ -1,71 +1,75 @@
 var easing = {
-    quadratic: function (x){
-      return Math.sqrt(x);
-    }
+  quadratic: function (x) {
+    return Math.sqrt(x);
+  }
 };
 
-function range(start, stop, step){
+function range(start, stop, step) {
   var array = [];
-  for(var i = start; i < stop; i += step) array.push(i);
+  for (var i = start; i < stop; i += step) array.push(i);
   return array;
 }
 
-function interpolation(fps, easing, finalValue){
-  function scaleIt(value){return finalValue * value; }
+function interpolation(fps, easing, finalValue) {
+  function scaleIt(value) { return finalValue * value; }
 
-  var x = range(0, 1, 1/fps),
-      y = x.map(easing).map(scaleIt);
+  var x = range(0, 1, 1 / fps),
+    y = x.map(easing).map(scaleIt);
 
   return y;
 }
 
-function animateEl(values, duration, onAnimate){
+function animateEl(values, duration, onAnimate) {
   var frameIndex = 0,
-      fps = values.length,
-      id = setInterval(anime, duration/fps );
+    fps = values.length,
+    id = setInterval(anime, duration / fps);
 
-  function anime(){
+  function anime() {
     var current = values[frameIndex],
-        isLastFrame = (frameIndex === fps - 1);
+      isLastFrame = (frameIndex === fps - 1);
 
     onAnimate(current, frameIndex, values);
 
-    if(isLastFrame){
+    if (isLastFrame) {
       clearInterval(id);
-    }else{
+    } else {
       frameIndex++;
     }
   }
 }
 
 function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
-function unformat(content){
+function unformat(content) {
   var unlocalized = content.replace('.', '').replace(',', '.'),
-      value = parseFloat(unlocalized);
+    value = parseFloat(unlocalized);
   return value;
 }
 
-function format(value){
+function format(value) {
   return value.toString().replace('.', ',');
 }
 
-window.addEventListener("DOMContentLoaded", function(){
-    var fps = 30,
-        els = [].slice.call(document.querySelectorAll('.slowNumber'));
+window.addEventListener("scroll", function slow() {
+  if (window.scrollY > 350) {
 
-    els.forEach(function(el){
-        var content = (el.firstChild.textContent).trim(),
-            decimalPlaces = content.split(',')[1] || '',
-            value = unformat(content),
-            values = interpolation(fps, easing.quadratic, value);
+    var fps = 20, els = [].slice.call(document.querySelectorAll('.slowNumber'));
 
-        animateEl(values, 1000, function (current, i, values){
-          var isLast = (i === values.length - 1),
-              value = round(current, decimalPlaces.length);
-          el.firstChild.textContent = isLast? content : format(value);
-        });
+    els.forEach(function (el) {
+      var content = (el.firstChild.textContent).trim(),
+        decimalPlaces = content.split(',')[1] || '',
+        value = unformat(content),
+        values = interpolation(fps, easing.quadratic, value);
+
+      animateEl(values, 1000, function (current, i, values) {
+        var isLast = (i === values.length - 1),
+          value = round(current, decimalPlaces.length);
+        el.firstChild.textContent = isLast ? content : format(value);
+      });
     });
-});
+
+    this.removeEventListener("scroll", slow)
+  }
+})
